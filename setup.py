39ef67cb650425bce2cf1407175f91f8b8706cb8
Ravi RT Merugu <rrmerugu@gmail.com>
2018-01-07 10:30:36 +0530

    setup.py added

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='web-crawler',
+      version='1.0',
+      description='A micro-framework to crawl the web pages - blogs/rss using defined configurations',
+      author='Ravi RT Merugu',
+      author_email='rrmerugu@gmail.com',
+      url='https://github.com/invaana/web-crawler',
+      packages=['webcrawler', ],
+      install_requires=['scrapy']
+      )
