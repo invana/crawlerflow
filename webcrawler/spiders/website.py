bad30fca34d55a2a0aba047b01aface61a213040
Ravi RT Merugu <rrmerugu@gmail.com>
2018-03-13 12:08:51 +0530

    website spider updated with conditions on the crawling - deny/allow patterns

--- webcrawler/spiders/website.py
+++ webcrawler/spiders/website.py
@@ -3,12 +3,18 @@ from scrapy.linkextractors import LinkExtractor
 
 
 class InvanaWebsiteSpider(CrawlSpider):
+    """
+
+    academics , faculty, department, research, fund, research proposals, funding proposals
+
+    """
     name = 'srmuniv.ac.in'
     allowed_domains = ['www.srmuniv.ac.in']
     start_urls = ['http://www.srmuniv.ac.in/']
 
     rules = (
-        Rule(LinkExtractor(), callback='parse_item', follow=True),
+        Rule(LinkExtractor(deny=(r'event-created/', r'event', r'content',r'career',
+                                 r'about', r'convocation', r'gallery')), callback='parse_item', follow=True,),
     )
 
     def parse_item(self, response):
