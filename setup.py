aff57e559a48bc93bdea97716621953894e8b772
Ravi RT Merugu <rrmerugu@gmail.com>
2018-03-18 23:15:17 +0530

    dependencies updated for setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='web-crawler-plus',
-      version='0.9.8',
+      version='0.9.9',
       description='A micro-framework to crawl the web pages with crawlers configs.'
                   ' It can use MongoDB, Elasticsearch and Solr databases to cache and save the extracted data.',
       author='Ravi Raja Merugu',
@@ -12,5 +12,6 @@ setup(name='web-crawler-plus',
       packages=['webcrawler', 'tests',
                 'webcrawler.spiders', 'webcrawler.pipelines', 'webcrawler.httpcache',
                 'webcrawler.spiders.search_engines', 'webcrawler.utils'],
-      install_requires=['scrapy', 'pysolr', 'pymongo', 'elasticsearch-dsl']
+      install_requires=['Scrapy==1.5.0', 'pysolr==3.7.0', 'pymongo==3.6.1', 'elasticsearch-dsl==6.1.0',
+                        "feedparser==5.2.1"]
       )
