abc7ec0a4e9ff0306d3cfca808c369b0ead536d7
Ravi Raja Merugu <rrmerugu@gmail.com>
2019-02-18 00:06:36 +0530

    fixes added for extracting multiple elements of text

--- invana_bot/utils/selectors.py
+++ invana_bot/utils/selectors.py
@@ -1,21 +1,35 @@
+def clean_data(elems=None, selector=None):
+    if selector.get('multiple'):
+        data_cleaned = []
+        data = elems.extract()
+        for i,datum in enumerate(data):
+            data_cleaned[i] = datum.strip() if datum else None
+        return data_cleaned
+    else:
+        data = elems.extract_first()
+        if data:
+            return data.strip()
+        return data
+
+
 def get_selector_element(html_element, selector, ):
     if selector.get('selector_attribute') in ['text']:
         if selector.get('selector_type') == 'css':
             elems = html_element.css("{0}::{1}".format(selector.get('selector'),
                                                        selector.get('selector_attribute')))
-            return elems.extract() if selector.get('multiple') else elems.extract_first()
+            return clean_data(elems=elems, selector=selector)
         else:
             raise NotImplemented("selector_type not equal to css; this is not implemented")
     elif selector.get('selector_attribute') == 'html':
         if selector.get('selector_type') == 'css':
             elems = html_element.css(selector.get('selector'))
-            return elems.extract() if selector.get('multiple') else elems.extract_first()
+            return clean_data(elems=elems, selector=selector)
         else:
             raise NotImplemented("selector_type not equal to css; this is not implemented")
     else:
         if selector.get('selector_type') == 'css':
             elems = html_element.css(selector.get('selector')) \
                 .xpath("@{0}".format(selector.get('selector_attribute')))
-            return elems.extract() if selector.get('multiple') else elems.extract_first()
+            return clean_data(elems=elems, selector=selector)
         else:
             raise NotImplemented("selector_type not equal to css; this is not implemented")
