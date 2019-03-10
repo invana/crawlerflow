2613b1a7e3117d02b44532fc59e2d68a151a4991
Ravi Raja Merugu <rrmerugu@gmail.com>
2019-03-10 06:25:10 +0530

    run a bot module implemention cleanups

--- bin/bot.py
+++ bin/bot.py
@@ -1,25 +1,41 @@
 import sys
 import os
+import argparse
 
 sys.path.append('../')
 from invana_bot.crawlers.generic import InvanaBotWebCrawler
 from invana_bot.settings import DEFAULT_SETTINGS
 from invana_bot.managers.manifest import ETIManifestManager
 
-path = os.path.abspath('../examples/basic')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='InvanaBot - A web crawler framework that can'
+                                                 ' transform websites into datasets with Crawl, '
+                                                 'Transform and Index workflow. ')
+    parser.add_argument('--path', dest='path', action='store_const',
+                        const=str, default='./',
+                        help='Path to the eti setup files (default: ./)')
 
-print("path", path)
-manifest_manager = ETIManifestManager(
-    cti_config_path=path
-)
-cti_manifest = manifest_manager.get_manifest()
-crawler = InvanaBotWebCrawler(
-    settings=DEFAULT_SETTINGS
-)
-context = cti_manifest.get("context")
-job = crawler.create_job(
-    cti_manifest=cti_manifest,
-    context=context
-)
-print("job", job)
-crawler.start_job(job=job)
+    args = parser.parse_args()
+    path = os.path.abspath(args.path)
+    manifest_manager = ETIManifestManager(
+        cti_config_path=path
+    )
+    cti_manifest, errors = manifest_manager.get_manifest()
+    if len(errors) == 0:
+        crawler = InvanaBotWebCrawler(
+            settings=DEFAULT_SETTINGS
+        )
+        context = cti_manifest.get("context")
+        job = crawler.create_job(
+            cti_manifest=cti_manifest,
+            context=context
+        )
+        print("job", job)
+        crawler.start_job(job=job)
+    else:
+        print("==============================================================")
+        print("ERROR : The path {} doesnt have the required files and failing with the errors {}".format(
+            manifest_manager.cti_config_path,
+            errors
+        ))
+        print("==============================================================")
