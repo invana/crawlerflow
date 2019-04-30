81d603910c9bc320cb000afdbffc4fa6d41b768c
Ravi Raja Merugu <rrmerugu@gmail.com>
2019-04-30 13:19:19 +0530

    xpath support added

--- invana_bot/utils/selectors.py
+++ invana_bot/utils/selectors.py
@@ -20,17 +20,23 @@ def get_selector_element(html_element, selector, ):
                                                        selector.get('selector_attribute')))
             return clean_data(elems=elems, selector=selector)
         else:
-            raise NotImplemented("selector_type not equal to css; this is not implemented")
+            elems = html_element.xpath("{0}/{1}".format(selector.get('selector'),
+                                                        selector.get('selector_attribute')))
+            return clean_data(elems=elems, selector=selector)
     elif selector.get('selector_attribute') == 'html':
         if selector.get('selector_type') == 'css':
             elems = html_element.css(selector.get('selector'))
             return clean_data(elems=elems, selector=selector)
         else:
-            raise NotImplemented("selector_type not equal to css; this is not implemented")
+            elems = html_element.xpath("{0}/{1}".format(selector.get('selector'),
+                                                        selector.get('selector_attribute')))
+            return clean_data(elems=elems, selector=selector)
     else:
         if selector.get('selector_type') == 'css':
             elems = html_element.css(selector.get('selector')) \
                 .xpath("@{0}".format(selector.get('selector_attribute')))
             return clean_data(elems=elems, selector=selector)
         else:
-            raise NotImplemented("selector_type not equal to css; this is not implemented")
+            elems = html_element.xpath("{0}/{1}".format(selector.get('selector'),
+                                                        selector.get('selector_attribute')))
+            return clean_data(elems=elems, selector=selector)
