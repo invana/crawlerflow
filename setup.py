085817fa7f2f29fa8d9e95ecb7458f4d9e16acf6
Ravi Raja Merugu <rrmerugu@gmail.com>
2019-07-23 14:07:25 +0530

    version incremented

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages, __version__ as setuptools_version
 
 
 setup(name='invana-bot',
-      version='0.1.31',
+      version='0.1.32',
       description='A web spider framework that can transform websites into datasets'
                   ' with Crawl, Transform and Index workflow.',
       author='Ravi Raja Merugu',
